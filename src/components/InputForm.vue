4cba160209e676a4fc26be59eca55168c34db450
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-09-25 15:44:04 +0700

    [Update] Wrapping Up

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,10 +1,11 @@
 <template>
-    <form>
+    <form @submit.prevent="submit">
         <label>Email :</label>
         <input type="email" required v-model="email">
 
         <label>Password</label>
         <input type="password" required v-model="password">
+        <span v-if="passwordError" class="error">{{ passwordError }}</span>
 
         <label>Role</label>
         <select v-model="role">
@@ -13,15 +14,26 @@
         </select>
 
         <label>Skills</label>
-        <input type="text" v-model="skills">
+        <input type="text" v-model="tempSkill" @keyup.ctrl="addSkill">
+        <div v-for="skill in skills" :key="skill" class="pill">
+            {{ skill }} <span @click="removeSkill(skill)">❌</span>
+        </div>
 
-        <label>ACCEPT TERMS AND CONDITION</label>
+        <div class="terms">
+            <input type="checkbox" v-model="terms">
+            <label>ACCEPT TERMS AND CONDITION</label>
+        </div>
+
+        <div class="submit">
+            <button>Submit</button>
+        </div>
     </form>
 
     <p>Email : {{ email }}</p>
     <p>Password : {{ password }}</p>
     <p>Role : {{ role }}</p>
     <p>Skills : {{ skills }}</p>
+    <p>Terms : {{ terms }}</p>
 </template>
 
 <script>
@@ -31,7 +43,29 @@ export default {
             email: "",
             password: "",
             role: "",
-            skills: []
+            tempSkill: '',
+            skills: [],
+            terms: false,
+            passwordError: ""
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if(e.key === "Enter" && this.tempSkill) {
+                this.skills.push(this.tempSkill)
+                this.tempSkill = ''
+            }
+        },
+        removeSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return item !== skill
+            })
+        },
+        submit() {
+            this.passwordError = this.password.length > 5 ? '' : "Password must be min 5 characters"
+            if (!this.passwordError) {
+                console.log("Berhasil Menambah Data")
+            }
         }
     }
 }
@@ -47,7 +81,7 @@ form {
     text-align: left;
 }
 label {
-    display: block;
+    display: inline-block;
     color: #aaa;
     font-size: 0.6em;
     margin: 25px 0 15px;
@@ -63,5 +97,40 @@ input, select {
     border-bottom: 1px solid black;
     color: #555;
     width: 100%;
+}
+input[type="checkbox"] {
+    display: inline-block;
+    position: relative;
+    width: 16px;
+    margin: 16px;
+    top: 2px;
+}
+.pill {
+    display: inline-block;
+    padding: 6px 12px;
+    background: #eee;
+    margin: 20px 10px 0 0;
+    border-radius: 20px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    font-size: 12px;
+    color: #477;
+    cursor: pointer;
+}
+.error {
+    color: red;
+    font-weight: bold;
+    display: block;
+}
+.submit {
+    text-align: center;
+}
+button {
+    border-radius: 30px;
+    padding: 10px 30px;
+    background:#477;
+    border: 0px;
+    color: white;
+    margin-top: 20px;
 }
 </style>
